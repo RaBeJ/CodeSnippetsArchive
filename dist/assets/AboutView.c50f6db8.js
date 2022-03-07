771577a8624201ce0a5862e216e9792b6429b251
RaBeJ <ralph.janz@gmx.de>
2022-03-07 04:36:41 +0100

    Added dist

---
+++ dist/assets/AboutView.c50f6db8.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.ab67a3ca.js";import{o as t,c as o,a as _}from"./vendor.a387885b.js";const s={},a={class:"about"},c=_("h1",null,"This is an about page",-1),n=[c];function r(i,u){return t(),o("div",a,n)}var f=e(s,[["render",r]]);export{f as default};
